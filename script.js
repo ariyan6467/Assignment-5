3246d12afdeca232603d0dbac05d587e5def6f8b
nahian jawad <nahianjawad4321@gmail.com>
2025-08-27 04:34:58 +0600

    call func. is over

--- script.js
+++ script.js
@@ -14,16 +14,15 @@ clearHistoryBtn.addEventListener("click",function(){
 
 let totaoHeart = 0 ;
 let heartIcon = document.querySelectorAll("#heartIcon");
-console.log(heartIcon);
+
 function amplifyHearts(heartIconsIndex) {
 
 heartIcon[heartIconsIndex].addEventListener("click", function(){
-    console.log("mama")
+    
     totaoHeart += 1;
-    console.log(totaoHeart);
-    console.log(typeof(totaoHeart));
+    
     let countHeart = document.querySelector("#countHeart");
-    console.log(parseInt(countHeart.innerText));
+    
     crntHeart = totaoHeart + parseInt(countHeart.innerText);
 
     countHeart.innerText = crntHeart ;
@@ -37,3 +36,47 @@ amplifyHearts(2);
 amplifyHearts(3);
 amplifyHearts(4);
 amplifyHearts(5);
+
+
+
+// CALL BUTTON FUNCTIONALITY
+
+let callBtn = document.querySelectorAll("#callBtn");
+
+let service = document.querySelectorAll("#service");
+let number = document.querySelectorAll("#number");
+let callIcon = document.querySelector("#callIcon");
+
+let totalCoin = 0 ;
+
+function  changecoins(callBtnIndex,serviceIndex,numberIndex){
+
+    callBtn[callBtnIndex].addEventListener("click", function(){
+   totalCoin += 20 ;
+    let coin = document.querySelector("#coin");
+    console.log(coin);
+         if(parseInt(coin.innerText) < totalCoin){
+            alert("you have not sufficient balance to call")
+         }else{
+                 let crntCoin =  parseInt(coin.innerText) - totalCoin ;
+
+                 coin.innerText = crntCoin ;
+
+                  totalCoin -= 20 ;
+
+    
+    
+                    alert(`${callIcon.innerText} calling ${service[serviceIndex].innerText}   ${number[numberIndex].innerText}....`)
+         }
+    
+})
+
+}
+
+
+changecoins(0,0,0);
+changecoins(1,1,1);
+changecoins(2,2,2);
+changecoins(3,3,3);
+changecoins(4,4,4);
+changecoins(5,5,5);
